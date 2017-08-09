1b1ecacf61f5b03add84c6d707cdeac7f608cca5
Nguyen Thi Thu <thunguyen0705@gmail.com>
2017-08-09 23:21:43 +0200

    updated

--- src/main/java/com/intilery/exercise/core/ecommerce/usecase/GetAnOrder.java
+++ src/main/java/com/intilery/exercise/core/ecommerce/usecase/GetAnOrder.java
@@ -6,19 +6,15 @@ import com.intilery.exercise.core.ecommerce.domain.OrderLines;
 import com.intilery.exercise.core.ecommerce.repository.UserGraphRepository;
 import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.Vertex;
+import com.tinkerpop.gremlin.java.GremlinPipeline;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
-import java.util.Set;
 
 import static com.tinkerpop.blueprints.Direction.IN;
-import static com.tinkerpop.blueprints.Direction.OUT;
 
 @Component
 public class GetAnOrder {
@@ -30,61 +26,57 @@ public class GetAnOrder {
         this.userGraphRepository = userGraphRepository;
     }
 
-    public Order getOrderByID(String email, String orderID) {
-        Vertex customer = userGraphRepository.getForUser(email);
-        return toOrder(customer, orderID);
+    public Order getOrder(String email, String orderID) {
+        Vertex vCustomer = userGraphRepository.getForUser(email);
+        return toOrder(vCustomer, orderID);
     }
 
-    private Order toOrder(final Vertex customer, String orderID) {
-        Vertex vertexOrder = getVertexOrder(customer, orderID);
-        Order order = getOrder(vertexOrder, orderID);
-        order.setEmail(customer.getProperty("email"));
-        return order;
-    }
-
-    private Vertex getVertexOrder(final Vertex customer, String orderID) {
-        Queue<Vertex> queue = new LinkedList<>();
-        queue.add(customer);
-        while (!queue.isEmpty()) {
-            Vertex v = queue.poll();
-            List<Edge> edges = (List<Edge>) v.getEdges(OUT);
-            if (!edges.isEmpty()) {
-                for (final Edge outEdge : edges) {
-                    Vertex ver = outEdge.getVertex(IN);
-                    if (ver.getProperty("type").equals("order") && ver.getProperty("orderID").equals(orderID)) {
-                        return ver;
-                    }
-                    queue.add(outEdge.getVertex(IN));
-                }
-            }
-        }
-        return null;
-    }
-
-    private Order getOrder(Vertex vertexOrder, String orderID) {
+    private Order toOrder(final Vertex vCustomer, String orderID) {
         Order order = new Order();
+        order.setEmail(vCustomer.getProperty("email"));
         List<OrderLine> lines = new ArrayList<>();
-        List<Edge> checkouts = (List<Edge>) vertexOrder.getEdges(IN);
-        List<Edge> products = (List<Edge>) checkouts.get(0).getVertex(OUT).getEdges(OUT);
-        for (Edge product : products) {
-            if(product.getLabel().equals("add to basket")) {
-                int qty = product.getProperty("qty") != null ? product.getProperty("qty") : 0;
-                Vertex p = product.getVertex(IN);
-                String image = p.getProperty("image");
-                double price = p.getProperty("price");
-                String name = p.getProperty("name");
-                OrderLine orderLine = new OrderLine();
-                orderLine.setName(name);
-                orderLine.setImage(image);
-                orderLine.setPrice(price);
-                orderLine.setQty(qty);
-                lines.add(orderLine);
-            }
+
+        List<DateTime> checkOutTimes = new ArrayList<>();
+        GremlinPipeline pipeOrder = new GremlinPipeline();
+        pipeOrder.start(vCustomer);
+        List<Vertex> vOrders = pipeOrder.outE("visit").inV().outE("check out").inV().toList();
+        for(Vertex vOrder: vOrders) {
+            List<Edge> eCheckOuts = (List<Edge>) vOrder.getEdges(IN, "check out");
+            DateTime checkOutTime = eCheckOuts.get(0).getProperty("createdAt");
+            checkOutTimes.add(checkOutTime);
         }
+
+        GremlinPipeline pipeProduct = new GremlinPipeline();
+        pipeProduct.start(vCustomer);
+        List<Vertex> vProducts = pipeProduct.outE("visit").inV().outE("add to basket").inV().toList();
+        for(Vertex product: vProducts) {
+            if(getAddToBasketTime(product).isAfterNow()) {
+                System.out.print("");
+            }
+            OrderLine orderLine = new OrderLine();
+            orderLine.setName(product.getProperty("name"));
+            orderLine.setImage(product.getProperty("image"));
+            orderLine.setPrice(product.getProperty("price"));
+            orderLine.setQty(getQty(product));
+            lines.add(orderLine);
+        }
+
         OrderLines orderLines = new OrderLines();
         orderLines.setOrderID(orderID);
         orderLines.setLines(lines);
         order.setOrder(orderLines);
         return order;
+    }
+
+    private int getQty(Vertex product) {
+        List<Edge> eAddToBaskets = (List<Edge>) product.getEdges(IN, "add to basket");
+        int qty = eAddToBaskets.get(0).getProperty("qty");
+        return qty;
+    }
+
+    private DateTime getAddToBasketTime(Vertex product) {
+        List<Edge> eAddToBaskets = (List<Edge>) product.getEdges(IN, "add to basket");
+        DateTime addToBasketTime = eAddToBaskets.get(0).getProperty("createdAt");
+        return addToBasketTime;
     }
 }
